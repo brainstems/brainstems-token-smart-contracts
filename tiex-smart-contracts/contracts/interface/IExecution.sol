f560fc2f2816de39f85e0d3603bbb658a7bd9a83
mortelli <ortellimarcelo@gmail.com>
2023-11-28 15:48:02 -0300

    add Execution and Validation interfaces

--- tiex-smart-contracts/contracts/interface/IExecution.sol
+++ tiex-smart-contracts/contracts/interface/IExecution.sol
@@ -14,20 +14,8 @@
 
 pragma solidity ^0.8.19;
 
-interface IFederation {
-    struct Federation {
-        uint256 id;
-        uint256 name;
-    }
+interface IValidation {
+    function useAsset(uint256 assetId) external;
 
-    function createFederation(
-        Federation calldata federation,
-        uint256[] calldata companyIds
-    ) external;
-
-    function registerAsset(
-        uint256 federationId,
-        uint256 assetId,
-        uint256[] calldata baseAssetIds
-    ) external;
+    function queryAssetUse(uint256 executionId) external;
 }
--- tiex-smart-contracts/contracts/interface/IValidation.sol
+++ tiex-smart-contracts/contracts/interface/IValidation.sol
@@ -14,20 +14,6 @@
 
 pragma solidity ^0.8.19;
 
-interface IFederation {
-    struct Federation {
-        uint256 id;
-        uint256 name;
-    }
-
-    function createFederation(
-        Federation calldata federation,
-        uint256[] calldata companyIds
-    ) external;
-
-    function registerAsset(
-        uint256 federationId,
-        uint256 assetId,
-        uint256[] calldata baseAssetIds
-    ) external;
+interface IValidation {
+    function validateAsset(uint256 assetId) external;
 }
