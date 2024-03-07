831de2f4ecf9b28ab87afd424bf26f89d1372076
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2024-03-07 10:25:04 -0300

    Add Deployment Scripts
    Update README.md
    
    Add Deployment Scripts
    
    Robust Scripts

---
+++ scripts/deploy_contracts.js
@@ -0,0 +1,136 @@
+const hre = require("hardhat");
+const { admin, usdcToken } = require("./config");
+
+async function main() {
+  if (process.env.DEPLOY_TOKEN === "true") {
+    await deployBrainstemsTokenContract();
+  }
+
+  if (process.env.DEPLOY_MEMBERSHIP === "true") {
+    await deployMembershipContract();
+  }
+
+  if (process.env.DEPLOY_ACCESS === "true") {
+    await deployAccessContract();
+  }
+
+  if (process.env.DEPLOY_ASSETS === "true") {
+    await deployAssetsContract();
+  }
+
+  if (process.env.DEPLOY_BRAINSTEM === "true") {
+    await deployBrainstemContract();
+  }
+
+  if (process.env.DEPLOY_EXECUTION === "true") {
+    await deployExecutionContract();
+  }
+
+  if (process.env.DEPLOY_VALIDATION === "true") {
+    await deployValidationContract();
+  }
+}
+
+async function deployBrainstemsTokenContract() {
+  console.log("deploying Brainstems Token Contract....");
+
+  const BrainstemsToken = await hre.ethers.getContractFactory(
+    "BrainstemsToken"
+  );
+  const brainstemsToken = await upgrades.deployProxy(BrainstemsToken, [
+    admin
+  ]);
+  await brainstemsToken.waitForDeployment();
+
+  console.log("deployed to :", await brainstemsToken.getAddress());
+}
+
+async function deployValidationContract() {
+  console.log("deploying Validation Contract....");
+
+  const Validation = await hre.ethers.getContractFactory(
+    "Validation"
+  );
+  const validation = await upgrades.deployProxy(Validation, [
+    // Constructor args.
+  ]);
+  await validation.waitForDeployment();
+
+  console.log("deployed to :", await validation.getAddress());
+}
+
+async function deployMembershipContract() {
+  console.log("deploying Membership Contract....");
+
+  const Membership = await hre.ethers.getContractFactory(
+    "Membership"
+  );
+  const membership = await upgrades.deployProxy(Membership, [
+    // Constructor args.
+  ]);
+  await membership.waitForDeployment();
+
+  console.log("deployed to :", await membership.getAddress());
+}
+
+async function deployExecutionContract() {
+  console.log("deploying Execution Contract....");
+
+  const Execution = await hre.ethers.getContractFactory(
+    "Execution"
+  );
+  const execution = await upgrades.deployProxy(Execution, [
+    // Constructor args.
+  ]);
+  await execution.waitForDeployment();
+
+  console.log("deployed to :", await execution.getAddress());
+}
+
+async function deployBrainstemContract() {
+  console.log("deploying Brainstem Contract....");
+
+  const Brainstem = await hre.ethers.getContractFactory(
+    "Brainstem"
+  );
+  const brainstem = await upgrades.deployProxy(Brainstem, [
+    // Constructor args.
+  ]);
+  await brainstem.waitForDeployment();
+
+  console.log("deployed to :", await brainstem.getAddress());
+}
+
+async function deployAssetsContract() {
+  console.log("deploying Assets Contract....");
+
+  const Assets = await hre.ethers.getContractFactory(
+    "Assets"
+  );
+  const assets = await upgrades.deployProxy(Assets, [
+    admin,
+    usdcToken
+  ]);
+  await assets.waitForDeployment();
+
+  console.log("deployed to :", await assets.getAddress());
+}
+
+async function deployAccessContract() {
+  console.log("deploying Access Contract....");
+
+  const Access = await hre.ethers.getContractFactory(
+    "Access"
+  );
+  const access = await upgrades.deployProxy(Access, [
+    // Constructor args.
+  ]);
+  await access.waitForDeployment();
+
+  console.log("deployed to :", await access.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/pre_deploy_contracts.js
@@ -0,0 +1,42 @@
+const { exec } = require("child_process");
+const minimist = require("minimist");
+const argv = minimist(process.argv.slice(2));
+const args = argv._;
+
+// Parse arguments
+if (!args.includes("--network")) {
+  console.error("No network specified. Use the --network flag to specify one.");
+  process.exit(1);
+}
+
+const env = {
+  DEPLOY_TOKEN: args.includes("-token"),
+  DEPLOY_MEMBERSHIP: args.includes("-membership"),
+  DEPLOY_ACCESS: args.includes("-access"),
+  DEPLOY_ASSETS: args.includes("-assets"),
+  DEPLOY_BRAINSTEM: args.includes("-brainstem"),
+  DEPLOY_EXECUTION: args.includes("-execution"),
+  DEPLOY_VALIDATION: args.includes("-validation"),
+  ...process.env, // Include existing environment variables
+};
+
+// Prepare the Hardhat command
+const network = args[args.indexOf("--network") + 1];
+if (!network || network === true || network.startsWith("-")) {
+  console.error("Wrong network specified. Use the --network flag to specify one like: '--network localhost'.");
+  process.exit(1);
+}
+
+const command = `npx hardhat run scripts/deploy_contracts.js --network ${network}`;
+
+// Execute the Hardhat script with environment variables
+exec(command, { env }, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+  }
+});
